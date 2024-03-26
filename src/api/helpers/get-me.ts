d5225b07c4f2d0844b4e4b8d03adabb4a2eda4a3
X_V.neer <xv.neer@gmail.com>
2024-03-26 17:12:33 +0200

    removed port

--- src/api/helpers/get-me.ts
+++ src/api/helpers/get-me.ts
@@ -3,8 +3,6 @@ import axios from "axios"
 import { UserResponse } from "@/types/user"
 
 export const getMe = async ({ token }: { token: string }) => {
-  const response = await axios.get<UserResponse>(
-    `http://127.0.0.1:3000/api/me?token=${token}`,
-  )
+  const response = await axios.get<UserResponse>(`/api/me?token=${token}`)
   return response.data
 }
