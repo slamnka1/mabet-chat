68a0dc15a75ae83b8bd5e533c679554376763435
Ahmed Yasser Salama <ahmedyassersalama2014@gmail.com>
2024-03-26 23:26:57 +0300

    Update get-chat.ts

--- src/api/helpers/get-chat.ts
+++ src/api/helpers/get-chat.ts
@@ -8,7 +8,7 @@ export const getChat = async <T>({
   token: string
 }) => {
   const response = await axios.get<T>(
-    `http://127.0.0.1:3000/api/chats/${chatID}?token=${token}`,
+    `https://127.0.0.1:3000/api/chats/${chatID}?token=${token}`,
   )
 
   return response.data
